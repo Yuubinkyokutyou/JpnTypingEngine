aa1ce2548f10792dac68385a29bf9ab40e08d174
yuubin <oonnii31@gmail.com>
2025-04-26 16:38:23 +0900

    リファクタ

--- Assets/JpnTypingEngine/Runtime/HiraganaSection.cs
+++ Assets/JpnTypingEngine/Runtime/HiraganaSection.cs
@@ -1,24 +1,28 @@
+#region
+
 using System.Collections.Generic;
+
+#endregion
 
 namespace JpnTypingEngine
 {
     /// <summary>
-    /// ひらがなの区切りとその入力組み合わせ
+    ///     ひらがなの区切りとその入力組み合わせ
     /// </summary>
     public class HiraganaSection
     {
         public string Hiragana { get; private set; }
 
         /// <summary>
-        /// 文章のひらがなの開始位置。「あいうえお」の「あ」だったら０
+        ///     文章のひらがなの開始位置。「あいうえお」の「あ」だったら０
         /// </summary>
         public int StartIndex { get; private set; }
-        
+
         /// <summary>
-        ///　Hiraganaが「い」の場合、入力組み合わせは「i」「yi」
+        ///     Hiraganaが「い」の場合、入力組み合わせは「i」「yi」
         /// </summary>
         public List<string> InputPairs { get; private set; }
-        
+
         public HiraganaSection(string hiragana, int startIndex, List<string> inputPairs)
         {
             Hiragana = hiragana;
--- Assets/JpnTypingEngine/Runtime/TypingGameSystem.cs
+++ Assets/JpnTypingEngine/Runtime/TypingGameSystem.cs
@@ -1,73 +1,85 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
 
+#endregion
+
 namespace JpnTypingEngine
 {
     /// <summary>
-    /// Converterの組み合わせをもとに、タイピングゲーム上での入力を受け取った結果を管理するクラス
+    ///     Converterの組み合わせをもとに、タイピングゲーム上での入力を受け取った結果を管理するクラス
     /// </summary>
-    public class TypingGameSystem :IDisposable
+    public class TypingGameSystem : IDisposable
     {
         public TypingInputResult TypingInputResult;
 
-        private HiraganaToInputConverter _hiraganaToInputConverter;
+        private readonly HiraganaToInputConverter _hiraganaToInputConverter;
         private InputCombination InputCombination => _hiraganaToInputConverter.InputCombination;
-        
+
         //予測の入力セクションを、入力順に格納
-        readonly List<HiraganaSection> _suggestHiraganaSections = new List<HiraganaSection>();
+        private readonly List<HiraganaSection> _suggestHiraganaSections = new();
+
         //現在のセクションであるHiraganaSection[0]のどのキーが選択されているか
         private string _currentSectionSelectKey;
+
         //現在のセクションの入力済みキー
-        readonly StringBuilder _currentSectionInputtedKey = new StringBuilder();
+        private readonly StringBuilder _currentSectionInputtedKey = new();
+
         //入力されたひらがな
-        readonly StringBuilder _inputtedHiragana = new StringBuilder();
+        private readonly StringBuilder _inputtedHiragana = new();
+
         //入力済みのキー（セクションが完了したときにアップデート）
-        readonly StringBuilder _inputtedSectionKeys = new StringBuilder();
+        private readonly StringBuilder _inputtedSectionKeys = new();
+
         //表示key
-        readonly StringBuilder _viewInputKeys = new StringBuilder();
+        private readonly StringBuilder _viewInputKeys = new();
+
         //表示、未入力のキー
-        readonly StringBuilder _viewNotInputKeys = new StringBuilder();
+        private readonly StringBuilder _viewNotInputKeys = new();
+
         //表示、入力済みのキー
-        readonly StringBuilder _viewInputtedKeys = new StringBuilder();
-        
+        private readonly StringBuilder _viewInputtedKeys = new();
+
 
         private const string HiraganaKeyPairListAssetPath = "HiraganaKeyPairList";
 
-        public TypingGameSystem(HiraganaInputMapping hiraganaInputMapping=null)
+        public TypingGameSystem(HiraganaInputMapping hiraganaInputMapping = null)
         {
             if (hiraganaInputMapping == null)
             {
                 hiraganaInputMapping = Resources.Load<HiraganaInputMapping>(HiraganaKeyPairListAssetPath);
             }
+
             _hiraganaToInputConverter = new HiraganaToInputConverter(hiraganaInputMapping);
         }
-        
-        
+
         public TypingInputResult InputKey(char key)
         {
             if (InputCombination == null)
             {
                 throw new Exception("値がセットされていません");
             }
+
             //問題が終了済みの場合
             if (_inputtedHiragana.Length == InputCombination.Hiragana.Length)
-            { 
+            {
                 throw new Exception("入力完了している状態で、入力されています。");
             }
-            
-            
-            TypingInputResult = new(
+
+
+            TypingInputResult = new TypingInputResult(
                 _currentSectionInputtedKey,
                 _inputtedHiragana,
                 _inputtedSectionKeys,
                 _viewInputKeys,
                 _viewInputtedKeys,
                 _viewNotInputKeys
-                );
-            
+            );
+
             //入力されたキーが、現在のセクションの選択キーと一致するか
             if (_currentSectionSelectKey[_currentSectionInputtedKey.Length] == key)
             {
@@ -82,7 +94,7 @@ namespace JpnTypingEngine
                     SetSuggestSections(_inputtedHiragana.Length, _currentSectionInputtedKey);
                     TypingInputResult.InputKeyChange = true;
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
                     //追加したキーが、ミスのため最後のキーを削除
                     _currentSectionInputtedKey.Remove(_currentSectionInputtedKey.Length - 1, 1);
@@ -102,14 +114,14 @@ namespace JpnTypingEngine
 // #endif
 //                 }
             }
-            
+
             //セクションの入力が完了した場合
             if (_currentSectionInputtedKey.Length == _currentSectionSelectKey.Length)
             {
                 _inputtedSectionKeys.Append(_currentSectionInputtedKey);
                 _currentSectionInputtedKey.Clear();
                 _inputtedHiragana.Append(_suggestHiraganaSections[0].Hiragana);
-                
+
                 //文章を入力し終えた場合
                 if (_inputtedHiragana.Length == InputCombination.Hiragana.Length)
                 {
@@ -119,14 +131,13 @@ namespace JpnTypingEngine
                 {
                     SetNextSection();
                 }
-                
             }
 
 
             SetViewKeys();
             return TypingInputResult;
         }
-        
+
         public TypingInputResult SetQuestion(string hiragana)
         {
             _inputtedHiragana.Clear();
@@ -135,11 +146,11 @@ namespace JpnTypingEngine
             _viewInputKeys.Clear();
 
             _hiraganaToInputConverter.Convert(hiragana);
-            
+
             SetSuggestSections(0);
-            
+
             SetViewKeys();
-            
+
             TypingInputResult = new TypingInputResult(
                 _currentSectionInputtedKey,
                 _inputtedHiragana,
@@ -148,15 +159,15 @@ namespace JpnTypingEngine
                 _viewInputtedKeys,
                 _viewNotInputKeys
             );
-            
+
             return TypingInputResult;
         }
-        
+
         /// <summary>
-        /// _suggestHiraganaSectionsに予測のセクションを格納
-        /// _currentSectionSelectKeyに次のセクションの選択キーを格納
+        ///     _suggestHiraganaSectionsに予測のセクションを格納
+        ///     _currentSectionSelectKeyに次のセクションの選択キーを格納
         /// </summary>
-        private void SetSuggestSections(int inputtedHiraganaLength,StringBuilder newSectionInputtedKey)
+        private void SetSuggestSections(int inputtedHiraganaLength, StringBuilder newSectionInputtedKey)
         {
             int suggestIndex = inputtedHiraganaLength;
 
@@ -177,15 +188,15 @@ namespace JpnTypingEngine
             _currentSectionSelectKey = _suggestHiraganaSections[0].InputPairs[0];
         }
 
-        
-        void SetRemainSections(int startHiraganaIndex)
+
+        private void SetRemainSections(int startHiraganaIndex)
         {
             while (startHiraganaIndex < InputCombination.Hiragana.Length)
             {
                 var hiraganaSections = InputCombination.GetHiraganaSections(startHiraganaIndex);
-                
-                if(hiraganaSections.Count == 0) throw new Exception("値が見つかりませんでした.");
-                
+
+                if (hiraganaSections.Count == 0) throw new Exception("値が見つかりませんでした.");
+
                 //入力ひらがな文字が0文字のセクションは割り当てない（nの例外の時のため）
                 HiraganaSection setSection = null;
                 foreach (var variable in hiraganaSections)
@@ -196,29 +207,30 @@ namespace JpnTypingEngine
                         break;
                     }
                 }
-                
+
                 _suggestHiraganaSections.Add(setSection);
                 startHiraganaIndex += setSection.Hiragana.Length;
             }
         }
-        
-        void SetNextSection()
+
+        private void SetNextSection()
         {
             _currentSectionSelectKey = _suggestHiraganaSections[1].InputPairs[0];
             _suggestHiraganaSections.RemoveAt(0);
         }
 
         //入力中のセクションで、入力されたキーに一致するセクションを取得
-        private (HiraganaSection section,string selectInputPair) GetSectionByInputKey(StringBuilder newSectionInputtedKey,int inputtedHiraganaLength)
+        private (HiraganaSection section, string selectInputPair) GetSectionByInputKey(
+            StringBuilder newSectionInputtedKey, int inputtedHiraganaLength)
         {
             int suggestIndex = inputtedHiraganaLength;
 
             var hiraganaSections = InputCombination.GetHiraganaSections(suggestIndex);
-            
+
             foreach (var hiraganaSection in hiraganaSections)
             {
                 foreach (var inputPair in hiraganaSection.InputPairs)
-                {                    
+                {
                     //入力済みより短い場合除外
                     if (inputPair.Length < newSectionInputtedKey.Length) continue;
 
@@ -228,7 +240,7 @@ namespace JpnTypingEngine
                         if (inputPair[i] != newSectionInputtedKey[i]) break;
                         if (i == newSectionInputtedKey.Length - 1)
                         {
-                            return (hiraganaSection,inputPair);
+                            return (hiraganaSection, inputPair);
                         }
                     }
                 }
@@ -236,14 +248,14 @@ namespace JpnTypingEngine
 
             throw new MissTypeException("タイプミスが発生しました。");
         }
-        
+
         private void SetViewKeys()
         {
             //１つ目は_currentSectionSelectKey
             _viewInputKeys.Clear();
             _viewInputKeys.Append(_inputtedSectionKeys);
             _viewInputKeys.Append(_currentSectionSelectKey);
-            for(int i = 1; i < _suggestHiraganaSections.Count; i++)
+            for (int i = 1; i < _suggestHiraganaSections.Count; i++)
             {
                 _viewInputKeys.Append(_suggestHiraganaSections[i].InputPairs[0]);
             }
@@ -251,19 +263,19 @@ namespace JpnTypingEngine
             _viewInputtedKeys.Clear();
             _viewInputtedKeys.Append(_inputtedSectionKeys);
             _viewInputtedKeys.Append(_currentSectionInputtedKey);
-            
+
             _viewNotInputKeys.Clear();
             _viewNotInputKeys.Append(_viewInputKeys);
             _viewNotInputKeys.Remove(0, _viewInputtedKeys.Length);
         }
-        
+
         public List<string> GetAllCombinations()
         {
             // StartIndexに基づいてHiraganaSectionをソート
             var sortedSections = _suggestHiraganaSections.OrderBy(section => section.StartIndex).ToList();
 
             // 初期化: 空のリストのリストから開始
-            var results = new List<List<string>> { new List<string>() };
+            var results = new List<List<string>> { new() };
 
             // SortedSectionsごとにループ
             foreach (var section in sortedSections)
@@ -287,16 +299,15 @@ namespace JpnTypingEngine
             // 最終的な組み合わせを文字列に結合して返す
             return results.Select(resultList => string.Join("", resultList)).ToList();
         }
-        
-        
+
+
         public void Dispose()
         {
             _hiraganaToInputConverter?.Dispose();
         }
     }
-    
-    
-    
+
+
     public class MissTypeException : Exception
     {
         public MissTypeException(string message)
